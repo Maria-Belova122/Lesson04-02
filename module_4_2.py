9edf70ae61ac3a6b42d8abc1d12cce18744ee046
Maria-Belova122 <mkb_maria@mail.ru>
2024-10-19 14:58:46 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,11 @@
+# ЗАДАНИЕ ПО ТЕМЕ "Пространство имен"
+
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+    inner_function()
+
+
+test_function()
+inner_function()
